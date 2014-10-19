09636c96b047a35cc68fa6f6ba1cf99baabb0a15
mbelev <martinbelev93@gmail.com>
2014-10-19 18:50:42 +0300

    Added the comments information in the email content if the subscriber wants to receive emails for comments. For every comment the comment text is given and there is also a link to the comment article.

--- week1/1-hackernews-scraper/hackernews-scraper-api/notifier/helpers/emailContentGenerator.js
+++ week1/1-hackernews-scraper/hackernews-scraper-api/notifier/helpers/emailContentGenerator.js
@@ -3,16 +3,28 @@
 module.exports = {
     /**
      * By given articles, the function generates the email content
-     * @param articles - All new articles for which subscriber is looking for
+     * @param items - All new articles and comments for which subscriber is looking for
      * @returns {string} Returns the whole email content which will be send to the subscribers
      */
-    generateEmailContent: function (articles) {
+    generateEmailContent: function (items) {
         var emailContent = 'Hello, you have subscribed for hacker news articles.\n';
-        emailContent += 'We have got new list of articles for you. Check the list below.\n';
+        emailContent += 'We have got new list of items for you. Check the list below.\n\n';
 
-        articles.forEach(function (article) {
-            emailContent += article.title + ' -> ' + article.url;
-        });
+        if (items.articles.length > 0) {
+            emailContent += 'New interesting articles:\n';
+            items.articles.forEach(function (article) {
+                emailContent += article.title + ' -> ' + article.url;
+            });
+
+            emailContent += '\n\n\n';
+        }
+
+        if (items.comments.length > 0) {
+            emailContent += 'New interesting comments with article url:\n';
+            items.comments.forEach(function (comment) {
+                emailContent += comment.text + ' -> ' + comment.url + '\n\n';
+            });
+        }
 
         return emailContent;
     }
