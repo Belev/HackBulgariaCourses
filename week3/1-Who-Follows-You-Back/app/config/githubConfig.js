c7690c1b66fd4ca5b2842fd37aa768b5eee36ee2
mbelev <martinbelev93@gmail.com>
2014-11-03 16:39:26 +0200

    Remove the whole github api url and added host and path.

--- week3/1-Who-Follows-You-Back/app/config/githubConfig.js
+++ week3/1-Who-Follows-You-Back/app/config/githubConfig.js
@@ -3,5 +3,6 @@
 module.exports = {
     clientId: '4b5719e5564afc453026',
     clientSecret: '2a7622152674a370f9565733f91ac85a1cc32f11',
-    githubApiUrl: '/users/:username/following?client_id=:client_id&client_secret=:client_secret'
+    host: 'api.github.com',
+    path: '/users/:username/following?client_id=:client_id&client_secret=:client_secret'
 };
