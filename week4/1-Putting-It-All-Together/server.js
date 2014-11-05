49fb87763906c8f28edb9643599487d207f69373
mbelev <martinbelev93@gmail.com>
2014-11-05 12:23:33 +0200

    Injected mongoose module to mongoose.js

--- week4/1-Putting-It-All-Together/server.js
+++ week4/1-Putting-It-All-Together/server.js
@@ -1,9 +1,10 @@
-var express = require('express'),
+var mongoose = require('mongoose'),
+    express = require('express'),
     app = express(),
     config = require('./config/config')['development'],
     SnippetController = require('./controllers/snippetsController');
 
-require('./config/mongoose')(config);
+require('./config/mongoose')(config, mongoose);
 require('./config/express')(app);
 require('./config/routes')(app, SnippetController);
 
