d515910d7898a77a2fdaf9cd9eabd4971efe9c16
mbelev <martinbelev93@gmail.com>
2014-11-06 11:21:32 +0200

    Removed unused comments. Uses the created config file. Added to every required component the mongoose module.

--- week4/1-Putting-It-All-Together/tests/routes-tests.js
+++ week4/1-Putting-It-All-Together/tests/routes-tests.js
@@ -6,45 +6,25 @@ var request = require('supertest'),
     mongoose = require('mongoose'),
     express = require('express'),
     app = express(),
-    config = {
-        port: 1470,
-        db: 'mongodb://localhost/snippets-tests'
-    },
+    config = require('./config/config'),
     snippetData = require('../data/snippet'),
     SnippetController = require('../controllers/snippetsController');
-
 
 require('../config/mongoose')(config, mongoose);
 require('../config/express')(app);
 require('../config/routes')(app, SnippetController);
 
-//app.listen(config.port);
-console.log('Server listenning on port: ' + config.port);
-
 request = request(app);
 
 describe('Server routes tests.', function () {
-//    var addedSnippet;
-//
-//    beforeEach(function (done) {
-//        snippetData.addSnipet({
-//            codeLanguage: 'test',
-//            createdBy: 'me',
-//            fileName: 'test',
-//            code: 'code'
-//        }).then(function (snippet) {
-//            addedSnippet = snippet;
-//            done();
-//        });
-//    });
-
     after(function () {
         mongoose.connection.db.dropDatabase();
     });
 
-    require('./routes-component-tests/post-create-snippet')(request);
-    require('./routes-component-tests/put-update-snippet')(request, snippetData);
-    require('./routes-component-tests/delete-delete-snippet')(request, snippetData);
-    require('./routes-component-tests/get-list-all-snippets')(request, snippetData);
-    require('./routes-component-tests/get-list-snippet-by-id')(request, snippetData);
+    require('./routes-component-tests/post-create-snippet')(request, mongoose);
+    require('./routes-component-tests/put-update-snippet')(request, snippetData, mongoose);
+    require('./routes-component-tests/delete-delete-snippet')(request, snippetData, mongoose);
+    require('./routes-component-tests/get-list-all-snippets')(request, snippetData, mongoose);
+    require('./routes-component-tests/get-list-snippet-by-id')(request, snippetData, mongoose);
+    require('./routes-component-tests/get-list-snippets-by-creator')(request, snippetData, mongoose);
 });
