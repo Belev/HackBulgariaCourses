f8d4c7b2784b7b75900796aa3be3092d77f3b5d6
mbelev <martinbelev93@gmail.com>
2014-11-06 10:29:47 +0200

    Delete route tests added.

---
+++ week4/1-Putting-It-All-Together/tests/routes-component-tests/delete-delete-snippet.js
@@ -0,0 +1,52 @@
+"use strict";
+
+module.exports = function (request, snippetData) {
+    describe('DELETE /delete_snippet tests.', function () {
+        var addedSnippet;
+
+        before(function (done) {
+            snippetData.addSnipet({
+                code: 'test',
+                codeLanguage: 'test-language',
+                createdBy: 'me',
+                fileName: 'test'
+            }).then(function (snippet) {
+                addedSnippet = snippet;
+                done();
+            });
+        });
+
+        it('Without provided id should return 400, text/html and "You must provide id."',
+            function (done) {
+                request.delete('/delete_snippet')
+                    .expect(400)
+                    .expect('Content-Type', /text\/html/)
+                    .expect('You must provide id.\n', done);
+            });
+
+        it('With invalid id should return 400, text/html and "There is no such snippet in the database."',
+            function (done) {
+                request.delete('/delete_snippet')
+                    .send({ id: 'invalid' })
+                    .expect(400)
+                    .expect('Content-Type', /text\/html/)
+                    .expect('There is no such snippet in the database.\n', done);
+            });
+
+        it('With valid id should return 200, application/json and the deleted snippet.',
+            function (done) {
+                request.delete('/delete_snippet')
+                    .send({id: addedSnippet.snippetId })
+                    .expect(200)
+                    .expect('Content-Type', /application\/json/)
+                    .end(function (err, res) {
+                        expect(res.body).not.to.be.empty;
+                        expect(res.body).to.have.property('codeLanguage').equal(addedSnippet.codeLanguage);
+                        expect(res.body).to.have.property('createdBy').equal(addedSnippet.createdBy);
+                        expect(res.body).to.have.property('fileName').equal(addedSnippet.fileName);
+                        expect(res.body).to.have.property('code').equal(addedSnippet.code);
+                        done();
+                    });
+            });
+    });
+};
