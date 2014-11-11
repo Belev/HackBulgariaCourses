05e022950df893e1fc05c3e9c6dcd9b385fd76a5
mbelev <martinbelev93@gmail.com>
2014-11-12 01:44:53 +0200

    Added areWordsReallyClose to know if the edit distance is 1 or 2 so that a group can be updated to fuzzy group.

--- exam1/1-Smart-Phonebook/utils/utils.js
+++ exam1/1-Smart-Phonebook/utils/utils.js
@@ -1,7 +1,13 @@
 "use strict";
 
+var Levenshtein = require('levenshtein');
+
 module.exports = {
     capitaliseFirstLetterAndLowerOthers: function (word) {
         return word[0].toUpperCase() + word.substr(1).toLowerCase();
+    },
+    areWordsReallyClose: function (firstWord, secondWord) {
+        var distance = new Levenshtein(firstWord, secondWord).distance;
+        return distance > 0 && distance <= 2;
     }
 };
