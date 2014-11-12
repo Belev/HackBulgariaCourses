c405151b880bacc916826276b2e7aef5fbd8e216
mbelev <martinbelev93@gmail.com>
2014-11-12 11:36:53 +0200

    Removed unused error handler middleware.

--- exam1/1-Smart-Phonebook/config/express.js
+++ exam1/1-Smart-Phonebook/config/express.js
@@ -6,9 +6,4 @@ var express = require('express'),
 module.exports = function (app) {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-
-    app.use(function(err, req, res, next) 	{
-        err.status = 400;
-        next(err);
-    });
 };
