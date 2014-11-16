8246fa1d6e115a0f76bfc6f80c753417c126a687
mbelev <martinbelev93@gmail.com>
2014-11-17 01:52:12 +0200

    Server for sitemap api added.

---
+++ exam1/3-Site-Mapper/server.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+    express = require('express'),
+    app = express(),
+    config = require('./config/config')['development'],
+    controllers = require('./controllers');
+
+require('./config/mongoose')(config, mongoose);
+require('./config/express')(app);
+require('./config/routes')(app, controllers);
+
+app.listen(config.port);
+
+console.log('Server listenning on port: ' + config.port);
