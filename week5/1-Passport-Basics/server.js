b4fc936510a6a9c2ef2320a743f25fb5162b60a2
mbelev <martinbelev93@gmail.com>
2014-11-17 18:57:43 +0200

    Server added.

---
+++ week5/1-Passport-Basics/server.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+    express = require('express'),
+    app = express(),
+    config = require('./config/config')['development'],
+    controllers = require('./controllers');
+
+require('./config/mongoose')(config, mongoose);
+require('./config/express')(app, config);
+require('./config/passport')();
+require('./config/routes')(app, controllers);
+
+app.listen(config.port);
+
+console.log('Server listenning on port: ' + config.port);
