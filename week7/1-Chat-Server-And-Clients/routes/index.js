c16f7d8a62f9d74e11641b53922c4b6dbbfdd3cb
mbelev <martinbelev93@gmail.com>
2014-11-28 22:25:26 +0200

    Deleted files because the code from them was extracted to views/includes, controllers and config folder.

--- week7/1-Chat-Server-And-Clients/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.index = function (req, res) {
-    res.render('index', {title: 'Express Chat'});
-};
-
-module.exports.chatroom = function (req, res) {
-    res.render('chatroom', {title: 'Express Chat'});
-};
